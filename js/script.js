0ab5c7f91cd7ea4e55dcbfeda60c53454027a4cc
adelorenz@gmail.com <adelorenz@gmail.com>
2022-05-14 16:35:31 +0000

    deploy: adelorenz/adsai-web-site@eec37e1b40399098c002a42869a03ad0ba37486f

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const mobileBtn = document.querySelector("button.navbar-toggle.collapsed");
 const menu = document.getElementById("navbar");
 mobileBtn.addEventListener("click", e => {
-    menu.style.display == "block" ? menu.style.display = "none" :  menu.style.display = "block"; 
+    menu.style.display = menu.style.display === "block" ?  "none" :   "block"; 
 });
+const dropdownicons = document.querySelectorAll(".nav-link i.bi.bi-caret-down-fill");
+
+ for (const element of dropdownicons) {
+    element.addEventListener("click", function(e) {
+        e.preventDefault();
+        let granny = element?.parentNode?.parentNode;
+        let menumobile = granny?.querySelector(".sub-menu");
+        menumobile.style.display = menumobile.style.display === "block" ?  "none" :   "block";
+     });
+ }
